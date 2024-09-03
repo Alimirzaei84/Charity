7910f48085ca52afa75cac3d8a4897e305fe751b
ali mirzaeif <mirzaeiali5000@gmail.com>
2024-09-03 15:15:05 +0330

    create models

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 class User(AbstractUser):
-    pass
+    GENDER_CHOICES = [
+        ('F', 'Female'),
+        ('M', 'Male'),
+    ]
+
+    address = models.CharField(blank=True, null=True, max_length=255)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, blank=True, null=True, max_length=1)
+    phone = models.CharField(blank=True, null=True, max_length=15)
--- charities/models.py
+++ charities/models.py
@@ -1,13 +1,44 @@
 from django.db import models
-
-
-class Benefactor(models.Model):
-    pass
+from django.db.models import SmallIntegerField, PositiveSmallIntegerField, IntegerField
 
 
 class Charity(models.Model):
-    pass
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
+
+
+class Benefactor(models.Model):
+    EXPERIENCE_CHOICES = [
+        (0, 'No Experience'),
+        (1, 'Some Experience'),
+        (2, 'Experienced'),
+    ]
+
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE)
+    experience = SmallIntegerField(choices=EXPERIENCE_CHOICES, default=0)
+    free_time_per_week = PositiveSmallIntegerField(default=0)
 
 
 class Task(models.Model):
-    pass
+    GENDER_CHOICES = [
+        ('F', 'Female'),
+        ('M', 'Male'),
+    ]
+
+    STATE_CHOICES = [
+        ('P', 'Pending'),
+        ('A', 'Assigned'),
+        ('W', 'Waiting'),
+        ('D', 'Done'),
+    ]
+
+    assigned_benefactor = models.ForeignKey('Benefactor', on_delete=models.SET_NULL, null=True)
+    charity = models.ForeignKey('Charity', on_delete=models.CASCADE)
+    age_limit_from = IntegerField(blank=True, null=True)
+    age_limit_to = IntegerField(blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    gender_limit = models.CharField(choices=GENDER_CHOICES, blank=True, null=True, max_length=1)
+    state = models.CharField(choices=STATE_CHOICES, default='P', max_length=1)
+    title = models.CharField(max_length=60)
